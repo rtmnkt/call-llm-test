bdcaf6737093e6a0d1e37abddc567aa0705a9307
ryota_munekata <98808444+rytmnkt@users.noreply.github.com>
2024-04-12 20:40:05 +0900

    test2

--- functions/index.ts
+++ functions/index.ts
@@ -1,7 +1,9 @@
 export const onRequest: PagesFunction<Env> = async (context) => {
   const url = new URL(context.request.url)
   if (url.searchParams.get('p') == 'test314') {
-    return fetch('/index2.html')
+    let res = await fetch('/index2.html');
+    res = new Response(res.body, res);
+    return res
   }
   return new Response('error')
 }
